14f374812cd4b3b413b509c1ca0ea0c520e563a0
alexjoe118 <debrajoycel8323@gmail.com>
2022-11-24 18:16:52 -0500

    fixing squid bet competiions logic

--- pages/bets/index.tsx
+++ pages/bets/index.tsx
@@ -1,11 +1,12 @@
 import { Box, Flex } from '@chakra-ui/react'
 import React, { useContext } from 'react'
 import BettingCardList from '../../components/bets/card/BettingCardList'
+import SquidCardList from '../../components/squid/SquidCardList'
 import HomeCurosel from '../../components/Container/Curosel'
 import { GAME } from '../../state/ActionTypes'
 import { AppContext } from '../../state/AppProvider'
 import { BettingData } from '../../Constants/BettingData'
-
+import { SquidData } from '../../Constants/SquidData'
 const bets = () => {
   const { state } = useContext(AppContext)
   return (
@@ -22,10 +23,14 @@ const bets = () => {
         <HomeCurosel />
         <Flex justifyContent={'center'} minHeight={'750px'}>
           <Box height={'auto'}>
-            <BettingCardList
-              gameType={state.game.game}
-              bettingData={BettingData}
-            />
+            {state.game.game == 'Squid Bet Competitions' ? (
+              <SquidCardList gameType={state.game.game} squidData={SquidData} />
+            ) : (
+              <BettingCardList
+                gameType={state.game.game}
+                bettingData={BettingData}
+              />
+            )}
           </Box>
         </Flex>
       </Box>
